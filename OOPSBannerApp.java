82c5d223eb24eed03df6e4e2c1ecfd21020362c7
hyndavi <sk7782@srmist.edu.in>
2026-02-24 10:11:39 +0530

    UC3: OOPS banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,56 @@
-/**
- * OOPSBannerApp
- * Prints the text "OOPS" as a large ASCII banner to the console.
- *
- * @author Hyndavi
- * @version 2.0
- */
-public class OOPSBannerApp {
+public class OopsBannerApp {
 
     public static void main(String[] args) {
+        printBanner();
+    }
 
-        System.out.println("  **    **    **    **  ");
-        System.out.println(" *     *  *     *  *     *  *     * ");
-        System.out.println(" *     *  *     *  *     *  *       ");
-        System.out.println(" *     *  *     *  *     *   ***  ");
-        System.out.println(" *     *  *     *  *     *        * ");
-        System.out.println(" *     *  *     *  *     *  *     * ");
-        System.out.println("  **    **    **    **  ");
+    // Static method to print OOPS banner
+    public static void printBanner() {
 
+        String line1 = String.join("",
+                "  OOOOO   ",
+                "  OOOOO   ",
+                "  PPPPPP  ",
+                "  PPPPPP  ",
+                "  SSSSSS  "
+        );
+
+        String line2 = String.join("",
+                " O     O  ",
+                " O     O  ",
+                " P     P ",
+                " P     P ",
+                " S       "
+        );
+
+        String line3 = String.join("",
+                " O     O  ",
+                " O     O  ",
+                " PPPPPP  ",
+                " PPPPPP  ",
+                "  SSSSS  "
+        );
+
+        String line4 = String.join("",
+                " O     O  ",
+                " O     O  ",
+                " P       ",
+                " P       ",
+                "       S "
+        );
+
+        String line5 = String.join("",
+                "  OOOOO   ",
+                "  OOOOO   ",
+                " P        ",
+                " P        ",
+                "  SSSSSS  "
+        );
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
     }
 }
