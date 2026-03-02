95d7a95ecb26ee77e8cc9350bebecce50500cc27
hyndavi <sk7782@srmist.edu.in>
2026-03-02 19:23:47 +0530

    Added OOPS banner program

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,56 +1,13 @@
-public class OopsBannerApp {
+/**
+ * OOPSBannerApp
+ * Prints the text "OOPS" to the console.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        printBanner();
-    }
-
-    // Static method to print OOPS banner
-    public static void printBanner() {
-
-        String line1 = String.join("",
-                "  OOOOO   ",
-                "  OOOOO   ",
-                "  PPPPPP  ",
-                "  PPPPPP  ",
-                "  SSSSSS  "
-        );
-
-        String line2 = String.join("",
-                " O     O  ",
-                " O     O  ",
-                " P     P ",
-                " P     P ",
-                " S       "
-        );
-
-        String line3 = String.join("",
-                " O     O  ",
-                " O     O  ",
-                " PPPPPP  ",
-                " PPPPPP  ",
-                "  SSSSS  "
-        );
-
-        String line4 = String.join("",
-                " O     O  ",
-                " O     O  ",
-                " P       ",
-                " P       ",
-                "       S "
-        );
-
-        String line5 = String.join("",
-                "  OOOOO   ",
-                "  OOOOO   ",
-                " P        ",
-                " P        ",
-                "  SSSSSS  "
-        );
-
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
+        System.out.println("OOPS");
     }
 }
