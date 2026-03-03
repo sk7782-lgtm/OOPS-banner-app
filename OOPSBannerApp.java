02e846f6ae9201d0e2c08c1d3efe4da35b628252
hyndavi <sk7782@srmist.edu.in>
2026-03-03 19:36:27 +0530

    UC4: Implemented banner using Array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,54 +2,20 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String row1 = String.join("   ",
-                " ***** ",
-                " ***** ",
-                " ******",
-                " ***** ");
+        String[] banner = {
 
-        String row2 = String.join("   ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *");
+            " *****     *****     *****      *****  ",
+            "*     *   *     *   *     *    *     * ",
+            "*     *   *     *   *     *    *        ",
+            "*     *   *     *   *****       *****   ",
+            "*     *   *     *   *               *   ",
+            "*     *   *     *   *         *     *    ",
+            " *****     *****    *          *****     "
 
-        String row3 = String.join("   ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*      ");
+        };
 
-        String row4 = String.join("   ",
-                "*     *",
-                "*     *",
-                "****** ",
-                " ***** ");
-
-        String row5 = String.join("   ",
-                "*     *",
-                "*     *",
-                "*      ",
-                "      *");
-
-        String row6 = String.join("   ",
-                "*     *",
-                "*     *",
-                "*      ",
-                "*     *");
-
-        String row7 = String.join("   ",
-                " ***** ",
-                " ***** ",
-                "*      ",
-                " ***** ");
-
-        System.out.println(row1);
-        System.out.println(row2);
-        System.out.println(row3);
-        System.out.println(row4);
-        System.out.println(row5);
-        System.out.println(row6);
-        System.out.println(row7);
+        for (String row : banner) {
+            System.out.println(row);
+        }
     }
 }
