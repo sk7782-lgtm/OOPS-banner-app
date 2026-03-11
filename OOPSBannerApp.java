8beb3d64e61157db77e0bbbd0ca3548f6fc3888e
hyndavi <sk7782@srmist.edu.in>
2026-03-11 10:08:34 +0530

    UC8: Use map for character patterns and render via function

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -8,9 +8,16 @@ import java.util.Map;
 
 public class OOPSBannerApp {
 
+    static Map<Character, String[]> patternMap = new HashMap<>();
+
     public static void main(String[] args) {
 
-        Map<Character, String[]> patternMap = new HashMap<>();
+        initializePatterns();
+
+        printBanner("OOPS");
+    }
+
+    public static void initializePatterns() {
 
         patternMap.put('O', new String[]{
                 " *** ",
@@ -36,15 +43,17 @@ public class OOPSBannerApp {
                 "**** "
         });
 
-        printBanner("OOPS", patternMap);
     }
 
-    public static void printBanner(String word, Map<Character, String[]> map) {
+    public static void printBanner(String word) {
 
         for (int i = 0; i < 5; i++) {
 
             for (char c : word.toCharArray()) {
-                System.out.print(map.get(c)[i] + " ");
+
+                String[] pattern = patternMap.get(c);
+                System.out.print(pattern[i] + " ");
+
             }
 
             System.out.println();
